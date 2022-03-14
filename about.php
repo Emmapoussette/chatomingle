437005e1eba71c9a21fae463ad283e78dc66a8b1
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-03-14 14:46:22 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,214 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> MingleFoReal | About :: Us
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+ <?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">About</li>
+     </ul>
+   </div>
+   <div class="about">
+   	  <div class="col-md-6 about_left">
+   	  	<img src="images/sawa.jpg" class="img-responsive" alt=""/>
+   	  </div>
+   	  <div class="col-md-6 about_right">
+   	  	<h1>About us</h1>
+   	  	<p>We Created This Platform To Link People With Purpose Of Your Long-Time Relationships, And Strengthen Connection With The Most Important People In Your Life</p>
+   	  	<div class="accordation">
+		   <div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">AddOn 1 <button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion-1-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion-1-" class="jb-accordion-content collapse in" style="height: auto;">
+				<p>Once you’ve entered your data, get your desire,interested person. One of the prominent benefits of online dating is that you get to know each other better before you meet.
+
+The chatting feature enables you to ask questions and interact through messages. It allows you to understand your match’s personality and interests.
+
+You can either pass or pursue if your personality is compatible. With time, you can exchange contacts and take your conversation on other social media platforms to get to know each other. </p>
+				</p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+			<div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">AddOn 2 <button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion2-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion2-" class="jb-accordion-content collapse ">
+				<p> The online world is often cruel. One wrong move, will not hesitate to take you down... Don't Mess Up!!!!  <br>                                                                  
+				MingleFoReal also organise a date trips as a part of face to face connection to better get know each other and get time to hear testimonies. 
+				</p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+			<div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">AddOn 3 <button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion2-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion2-" class="jb-accordion-content collapse ">
+				<p> The online world is often cruel. One wrong move, will not hesitate to take you down... Don't Mess Up!!!!  <br>                                                                  
+				MingleFoReal also organise a date trips as a part of face to face connection to better get know each other and get time to hear testimonies. 
+				</p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+		</div>
+   	  </div>
+   	  <div class="clearfix"> </div>
+   </div>
+  </div>
+</div><!--
+<div class="about_middle">
+	<div class="container">
+	  <h2>Happy Clients</h2>
+	  <div class="about_middle-grid1">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">hhdjnkjsdndsjbjbhfjhfbshfgsjdnfaklsjfhdyfbdcksljfiusgdbjs dv</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Full client testimonials names</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#"></a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Full client testimonials names </span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	  
+		<div class="clearfix"> </div>
+	  </div>
+	</div>
+</div>-->
+<div class="about_bottom">
+	<div class="container">
+		<h3>Support Team</h3>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="pChief Operating Officerosts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src=""  class="img-responsive" alt=""/></a>
+					<!--<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>-->
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Chief Operating Office</a></h4>
+			    	<p>Tel:0723617643</p>
+					<p>pcomfyltd@gmail.com</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src=""  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Customer service</a></h4>
+			    	<p>07888888888</p>
+					<p>mingleforeal@gmail.com</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src=""  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Relationship Adviso</a></h4>
+			    	<p>Relationship Advisor</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src=""  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="clearfix"> </div>
+	</div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
+</body>
+</html>	
